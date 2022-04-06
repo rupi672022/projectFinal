a65e2a6e43fafde0d56d29d5aa31ef3d69766021
noycohen10 <95696544+rupi672022@users.noreply.github.com>
2022-04-06 10:58:02 +0300

    update

--- Jacobs/Models/FindingPaths.cs
+++ Jacobs/Models/FindingPaths.cs
@@ -44,12 +44,13 @@ namespace System.Models
             FindingPathDataServices ds = new FindingPathDataServices();
             List<FindingPaths> FindingPathslist = ds.Read(date);
             return Algo(FindingPathslist, date);
-
+            //return FindingPathslist;
         }
 
 
         public List<FindingPaths> Algo(List<FindingPaths> list, string date)
         {
+            List<FindingPaths> selectedPath = new List<FindingPaths>();
 
             List<String> north = new List<string>();
             List<String> south = new List<string>();
@@ -92,35 +93,38 @@ namespace System.Models
 
             for (int i = 0; i < areasArr.Length; i++)
             {
-                if (areasArr[i].Count != 0)
+                if (areasArr[i].Count == 4)
                 {
                     var func = new fullfunc();
-                    func.factorial(areasArr[i].Count);
+                    int factRes = func.factorial(areasArr[i].Count);
+                    string[,] result = new string[factRes, (areasArr[i].Count) + 2];
                     List<String> newarr = new List<string>();
                     newarr.Insert(0, "גשר העץ 27,עמק חפר");
                     newarr.AddRange(areasArr[i]);
-                    List<String> dicArr = new List<string>();
+                    //List<String> dicArr = new List<string>();
                     string[] listToDicArr = newarr.ToArray();
                     string[] listToArr = areasArr[i].ToArray();
                     var resultDic = new Dictionary<string, int>();
                     resultDic = func.Dic(listToDicArr);
                     Console.Write("\nAll the combination are: \n", listToArr.Length);
-                    func.prnPermut(listToArr, 0, listToArr.Length - 1);
+                    func.prnPermut(listToArr, 0, listToArr.Length - 1, result);
                     Console.Write("\n\n");
-                    func.Dis(resultDic);
+                    selectedPath = func.Dis(resultDic, result);
+
                 }
             }
+            return selectedPath;
         }
 
 
         class fullfunc
         {
             int currRow = 0;
-            string[,] result = new string[120, 7];
 
-            public Dictionary<string, int> Dic(string [] areasArr)
+
+            public Dictionary<string, int> Dic(string[] areasArr)
             {
-              
+
                 var capitals = new Dictionary<string, int>();
                 for (int i = 0; i < areasArr.Length; i++)
                 {
@@ -130,12 +134,13 @@ namespace System.Models
                 return capitals;
             }
 
-            public void Dis(Dictionary<string, int> capitals)///להפוך לרשימה
+            public List<FindingPaths> Dis(Dictionary<string, int> capitals, string[,] result)///להפוך לרשימה
             {
+                List<FindingPaths> selectedCombintion = new List<FindingPaths>();
                 int indexWinner = 0;
                 int winner = 0;
                 int count = 0, sumline = 0;
-                int[,] arrayDis = new int[,] { { 0, 4, 2, 3 }, { 3, 0, 4, 5 }, { 5, 4, 0, 2 }, { 4, 3, 5, 0 } };
+                int[,] arrayDis = new int[,] { { 0, 5, 5, 4, 5 }, { 0, 5, 5, 4, 5 }, { 0, 5, 5, 4, 5 }, { 0, 5, 5, 4, 5 }, { 0, 5, 5, 4, 5 } };
                 List<int> termsList = new List<int>();
                 for (int i = 0; i < result.GetLength(0); i++)
                 {
@@ -174,17 +179,16 @@ namespace System.Models
                 Console.Write("The selected path is: ");
                 for (int j = 0; j < result.GetLength(1); j++)
                 {
-                    if (j == result.GetLength(1) - 1)
+                    foreach (FindingPaths obj in selectedCombintion)
                     {
-                        Console.Write(result[indexWinner, j] + " ");
+                        if (result[indexWinner, j] == obj.Address)
+                        {
+                            selectedCombintion.Add(new FindingPaths(obj.Address, obj.Lng, obj.Lat, obj.DistributaionArea, obj.DateArrivel, obj.CompanyNum));
+                        }
 
                     }
-                    else
-                    {
-                        Console.Write(result[indexWinner, j] + " -> ");
-                    }
                 }
-                Console.Write("And its length: " + winner + " km ");
+                return selectedCombintion;
 
             }
             public int factorial(int num)
@@ -196,7 +200,7 @@ namespace System.Models
                 }
                 return fact;
             }
-            public void prnPermut(string [] list, int k, int m)
+            public void prnPermut(string[] list, int k, int m, string[,] result)
             {
                 int i;
                 int Z = 1;
@@ -236,7 +240,7 @@ namespace System.Models
                     for (i = k; i <= m; i++)
                     {
                         SwapTwostring(ref listToArr[k], ref listToArr[i]);
-                        prnPermut(list, k + 1, m);
+                        prnPermut(list, k + 1, m, result);
                         SwapTwostring(ref listToArr[k], ref listToArr[i]);
                     }
                 }
@@ -268,4 +272,4 @@ namespace System.Models
 
 
 }
-    
+
